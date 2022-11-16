70c4d7e7f339502f30ed6e115a69b916252d1eff
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-16 08:37:25 +0000

    Adding tarfile member sanitization to extractall()

--- python/hetu/data.py
+++ python/hetu/data.py
@@ -65,7 +65,26 @@ def cifar10(directory='CIFAR_10', onehot=True):
             url = "https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz"
             urllib.request.urlretrieve(url, filename, reporthook=gen_bar_updater())
         with tarfile.open(filename, 'r:gz') as tar:
-            tar.extractall(path=directory)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, path=directory)
 
     images, labels = [], []
     for filename in file_lists[:5]:
@@ -123,7 +142,26 @@ def cifar100(directory='CIFAR_100', onehot=True):
             url = "https://www.cs.toronto.edu/~kriz/cifar-100-python.tar.gz"
             urllib.request.urlretrieve(url, filename, reporthook=gen_bar_updater())
         with tarfile.open(filename, 'r:gz') as tar:
-            tar.extractall(path=directory)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, path=directory)
 
     with open(file_lists[0], 'rb') as input_file:
         train_file = pickle.load(input_file, encoding='latin1')
